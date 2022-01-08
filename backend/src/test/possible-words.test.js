89c46c2bc2fcbc928ee6400b51bfebdd2f714dbb
nisha <nishavirdee@gmail.com>
2022-01-08 17:03:25 +0000

    add converter and tests

---
+++ backend/src/test/possible-words.test.js
@@ -0,0 +1,63 @@
+var pw = require('../possible-words.js');
+var assert = require('assert');
+
+describe("convertNumberToWords", () => {
+  it("correctly converts one digit", () => {
+    const result = pw.convertNumberToWords(["2"]);
+    const expected = ["a", "b", "c"];
+    assert.deepEqual(result, expected);
+  })
+
+  it("correctly converts two digits that are the same", () => {
+    const result = pw.convertNumberToWords(["2", "2"]);
+    const expected = ["aa", "ba", "ca", "ab", "bb", "cb", "ac", "bc", "cc"];
+    assert.deepEqual(result, expected);
+  })
+
+  it("correctly converts two digits that are different", () => {
+    const result = pw.convertNumberToWords(["2", "3"]);
+    const expected = ["ad", "bd", "cd", "ae", "be", "ce", "af", "bf", "cf"];
+    assert.deepEqual(result, expected);
+  })
+
+  it("correctly converts three digits", () => {
+    const result = pw.convertNumberToWords(["2", "2", "8"]);
+    const expected = [
+      "aat",
+      "bat",
+      "cat",
+      "abt",
+      "bbt",
+      "cbt",
+      "act",
+      "bct",
+      "cct",
+      "aau",
+      "bau",
+      "cau",
+      "abu",
+      "bbu",
+      "cbu",
+      "acu",
+      "bcu",
+      "ccu",
+      "aav",
+      "bav",
+      "cav",
+      "abv",
+      "bbv",
+      "cbv",
+      "acv",
+      "bcv",
+      "ccv"
+    ];
+    assert.equal(result.length, 27);
+    assert.deepEqual(result, expected);
+  })
+
+  it("correctly converts multiple digits", () => {
+    const result = pw.convertNumberToWords(["4", "3", "5", "5", "6"]);
+    const expected = ["ad", "bd", "cd", "ae", "be", "ce", "af", "bf", "cf"];
+    assert.equal(result.length, 243)
+  })
+})
